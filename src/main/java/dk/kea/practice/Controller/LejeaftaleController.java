33e6065eedf6260c66a4d383308009a4d0d21486
Yudo95 <yudo0001@stud.kea.dk>
2024-10-13 14:29:26 +0200

    Opret lejeaftale form: Dropdown (select) for Bil og User færdig

--- src/main/java/dk/kea/practice/Controller/LejeaftaleController.java
+++ src/main/java/dk/kea/practice/Controller/LejeaftaleController.java
@@ -1,12 +1,14 @@
 package dk.kea.practice.Controller;
 
+import dk.kea.practice.Model.Bil;
 import dk.kea.practice.Model.User;
+import dk.kea.practice.Service.BilService;
 import dk.kea.practice.Service.UserService;
-import org.springframework.ui.Model; // Use the correct import for Model
 import dk.kea.practice.Model.Lejeaftale;
 import dk.kea.practice.Service.LejeaftaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +19,28 @@ public class LejeaftaleController {
 
     private final LejeaftaleService lejeaftaleService;
     private final UserService userService;
+    private final BilService bilService;
 
     @Autowired
-    public LejeaftaleController(LejeaftaleService lejeaftaleService, UserService userService) {
+    public LejeaftaleController(LejeaftaleService lejeaftaleService, UserService userService, BilService bilService) {
         this.lejeaftaleService = lejeaftaleService;
         this.userService = userService;
+        this.bilService = bilService;
+    }
+
+    // Endpoint to get all lejeaftaler
+    @GetMapping
+    public String getAllLejeaftaler(Model model) {
+
+        List<Lejeaftale> lejeaftaler = lejeaftaleService.getAllLejeaftale();
+        List<Bil> biler = bilService.getAllBiler();
+        List<User> users = userService.getAllUsers();
+
+        model.addAttribute("biler", biler);
+        model.addAttribute("users", users);
+        model.addAttribute("lejeaftaler", lejeaftaler);// Add the list to the model
+
+        return "lejeaftaler"; // Return the view name to render
     }
 
     // Endpoint to create a new lejeaftale
@@ -30,23 +49,6 @@ public class LejeaftaleController {
         lejeaftaleService.createLejeaftale(lejeaftale);
         return "redirect:/lejeaftaler"; // Redirect to a page (e.g., a list of lejeaftaler)
     }
-
-    // New method to show the form for creating a lejeaftale
-    @GetMapping("/create")
-    public String showCreateLejeaftaleForm(Model model) {
-        List<User> users = userService.getAllUsers(); // Fetch all users
-        model.addAttribute("users", users); // Add users to the model
-        return "redirect:/lejeaftaler"; // Return the create view name
-    }
-
-    // Endpoint to get all lejeaftaler
-    @GetMapping
-    public String getAllLejeaftaler(Model model) {
-        List<Lejeaftale> lejeaftaler = lejeaftaleService.getAllLejeaftale();
-        model.addAttribute("lejeaftaler", lejeaftaler); // Add the list to the model
-        return "lejeaftaler"; // Return the view name to render
-    }
-
 
     @PostMapping("/delete")
     public String deleteLejeaftale(@RequestParam("id") int lejeaftale_id) {
@@ -74,5 +76,4 @@ public class LejeaftaleController {
 
         return "redirect:/lejeaftaler"; // Redirect to list of lejeaftaler after update
     }
-
 }
